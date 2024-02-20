d0703fed045a28da3b96d37fd8ed3585e03a1f07
Andres Salumets <andres.workmail@gmail.com>
2024-02-20 12:24:24 +0000

    Create an html for booking creation and modify urls in order for access to it

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 app_name = 'booking'
 urlpatterns = [
     path('', views.BookingList.as_view(), name="home"),
-    path('<int:pk>/', views.BookingDetail.as_view(), name="details"),    
+    path('<int:pk>/', views.BookingDetail.as_view(), name="details"),
+    path('create/', views.CreateBooking.as_view(), name="create_booking")    
 ]
