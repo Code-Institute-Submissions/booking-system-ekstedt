bc4fb5a75b222f69772de0264618eff08ed9dc5f
Andres Salumets <andres.workmail@gmail.com>
2024-02-23 15:58:29 +0000

    Update signup.html

--- booking/forms.py
+++ booking/forms.py
@@ -5,7 +5,7 @@ from .models import Booking
 class BookingForm (forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['username', 'customer_name', 'customer_email', 'date', 'start_time', 'end_time', 'party_size', 'table', 'notes']
+        fields = ['customer_name', 'customer_email', 'date', 'start_time', 'end_time', 'party_size', 'table', 'notes']
         exclude = ['created_at']
         widgets = {
             'date': DateInput(attrs={'type': 'date'}),
