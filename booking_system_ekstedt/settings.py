2b5272af6248c5e22a43fd2fdcfc63edfcbb58f9
Andres Salumets <andres.workmail@gmail.com>
2024-03-12 10:29:37 +0000

    small adjustment in settings

--- booking_system_ekstedt/settings.py
+++ booking_system_ekstedt/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-flyhighhher-bookingsyst-6z0zzwg7l35.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-flyhighhher-bookingsyst-6z0zzwg7l35.ws-eu108.gitpod.io', '.heroku.com']
 
 
 # Application definition
