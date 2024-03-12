48bb020aa81a23c4f3b68e787ab46b1585c42840
Andres Salumets <andres.workmail@gmail.com>
2024-03-12 13:20:17 +0000

    Fix Cloudinary settings

--- booking_system_ekstedt/settings.py
+++ booking_system_ekstedt/settings.py
@@ -136,11 +136,10 @@ USE_I18N = True
 USE_TZ = True
 
 # Cloudinary Storage Configuration
-CLOUDINARY_STORAGE = {
-    CLOUD_NAME: os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    API_SECRET: os.environ.get("CLOUDINARY_API_SECRET"),
-    API_KEY: os.environ.get("CLOUDINARY_API_KEY"),
-}
+
+CLOUD_NAME: os.environ.get("CLOUDINARY_CLOUD_NAME")
+API_SECRET: os.environ.get("CLOUDINARY_API_SECRET")
+API_KEY: os.environ.get("CLOUDINARY_API_KEY")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
